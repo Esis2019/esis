57c3a2bb46c7731c527f66382307014325eefc3a
= <=>
2019-11-06 08:42:23 -0500

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-hola mundo
+<?php
+require_once "models/enlaces.php";
+require_once "models/crud.php";
+require_once "controllers/controller.php";
+
+$mvc = new MvcController();
+$mvc -> pagina();
+
+?>
